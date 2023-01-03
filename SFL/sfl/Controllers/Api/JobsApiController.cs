dccf82fcc4e42e72f60b0671c44ab0eb46c40ca9
LukaSveigl <lukasveigl2708@gmail.com>
2023-01-03 23:19:10 +0100

    Updated JobsApiController PUT method to only update job statuses.

--- SFL/sfl/Controllers/Api/JobsApiController.cs
+++ SFL/sfl/Controllers/Api/JobsApiController.cs
@@ -89,7 +89,7 @@ namespace sfl.Controllers_Api
             //_context.Jobs.Attach(job);
             job.JobStatusID = jobStatusID;
             _context.Update(job);
-            _context.Entry(job).Property(j => j.JobStatusID).IsModified = true;
+            //_context.Entry(job).Property(j => j.JobStatusID).IsModified = true;
 
             try
             {
